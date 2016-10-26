39edbd8389af6b22027bc318d6e7dfdaf5dbceab
Timothy Klim <klimtimothy@gmail.com>
2016-10-26 23:09:30 +0300

    Add scala 2.12 & upgrade cats to 0.8.0

---
+++ src/main/scala/cats/scalatest/EitherMatchers.scala
@@ -0,0 +1,74 @@
+package cats.scalatest
+
+import org.scalatest.matchers.{ MatchResult, BeMatcher, Matcher }
+
+import scala.util.Either
+import cats.syntax.either._
+
+trait EitherMatchers {
+  /**
+   * Checks to see if `scala.util.Either` is a specific Left element.
+   */
+  def beLeft[E](element: E): Matcher[E Either _] = new BeCatsLeftEither[E](element)
+
+  /**
+   * Checks to see if `scala.util.Either` is a `Left`.
+   */
+  def left[E]: BeMatcher[E Either _] = new IsCatsLeftEitherMatcher[E]
+
+  /**
+   * Checks to see if `scala.util.Either` is a specific Right element.
+   */
+  def beRight[T](element: T): Matcher[_ Either T] = new BeCatsRightEitherMatcher[T](element)
+
+  /**
+   * Checks to see if `scala.util.Either` is a `Right`.
+   */
+  def right[T]: BeMatcher[_ Either T] = new IsCatsRightEitherMatcher[T]
+}
+
+/**
+ * Import singleton in case you prefer to import rather than mix in.
+ * {{{
+ * import EitherMatchers._
+ * result should beRight(100)
+ * }}}
+ */
+final object EitherMatchers extends EitherMatchers
+
+final private[scalatest] class BeCatsRightEitherMatcher[T](element: T) extends Matcher[_ Either T] {
+  def apply(either: _ Either T): MatchResult = {
+    MatchResult(
+      either.fold(_ => false, _ == element),
+      s"'$either' did not contain an Right element matching '$element'.",
+      s"'$either' contained an Right element matching '$element', but should not have."
+    )
+  }
+}
+
+final private[scalatest] class BeCatsLeftEither[E](element: E) extends Matcher[E Either _] {
+  def apply(either: E Either _): MatchResult = {
+    MatchResult(
+      either.fold(_ == element, _ => false),
+      s"'$either' did not contain an Left element matching '$element'.",
+      s"'$either' contained an Left element matching '$element', but should not have."
+    )
+  }
+}
+
+final private[scalatest] class IsCatsLeftEitherMatcher[E] extends BeMatcher[E Either _] {
+  def apply(either: E Either _): MatchResult = MatchResult(
+    either.isLeft,
+    s"'$either' was not an Left, but should have been.",
+    s"'$either' was an Left, but should *NOT* have been."
+  )
+}
+
+final private[scalatest] class IsCatsRightEitherMatcher[T] extends BeMatcher[_ Either T] {
+  def apply(either: _ Either T): MatchResult = MatchResult(
+    either.isRight,
+    s"'$either' was not an Right, but should have been.",
+    s"'$either' was an Right, but should *NOT* have been."
+  )
+}
+
---
+++ src/main/scala/cats/scalatest/EitherValues.scala
@@ -0,0 +1,85 @@
+package cats.scalatest
+
+import org.scalatest.exceptions.{ TestFailedException, StackDepthException }
+
+import cats.syntax.either._
+import org.scalactic.source
+import scala.util.{ Either, Right, Left }
+
+trait EitherValues {
+  import scala.language.implicitConversions
+
+  /**
+   * Implicit conversion that adds a `value` method to `scala.util.Either`
+   *
+   * @param either the `scala.util.Either` on which to add the `value` method
+   */
+  implicit def convertEitherToEitherable[E, T](either: E Either T)(implicit pos: source.Position): Eitherable[E, T] = new Eitherable(either, pos)
+
+  /**
+   * Container class for matching success
+   * type stuff in `scala.util.Either` containers,
+   * similar to `org.scalatest.OptionValues.Valuable`
+   *
+   * Meant to allow you to make statements like:
+   *
+   * <pre class="stREPL">
+   *   result.value should be &gt; 15
+   * </pre>
+   *
+   * Where it only matches if result is `Valid` and is also greater than 15.
+   *
+   * Otherwise your test will fail, indicating that it was left instead of right
+   *
+   * @param either A `scala.util.Either` object to try converting to a `Eitherable`
+   *
+   * @see org.scalatest.OptionValues.Valuable
+   */
+  final class Eitherable[E, T](either: E Either T, pos: source.Position) {
+    /**
+     * Extract the `Right` from the Either. If the value is not a right the test will fail.
+     */
+    def value: T = {
+      either match {
+        case Right(right) => right
+        case Left(left) =>
+          throw new TestFailedException((_: StackDepthException) => Some(s"'$left' is an Left, expected an Right."), None, pos)
+      }
+    }
+
+    /**
+     * Use .leftValue on an Either to extract the left side. Like .value, but for the left.
+     * If the value is a right, the test will fail.
+     */
+    def leftValue: E = {
+      either match {
+        case Right(right) =>
+          throw new TestFailedException((_: StackDepthException) => Some(s"'$right' is Valid, expected Invalid."), None, pos)
+        case Left(left) => left
+      }
+    }
+  }
+}
+
+/**
+ *
+ * Companion object for easy importing – rather than mixing in – to allow `EitherValues` operations.
+ *
+ * This will permit you to invoke a `value` method on an instance of a `scala.util.Either`,
+ * which attempts to unwrap the `Valid`.
+ *
+ * Similar to `org.scalatest.OptionValues.Valuable`
+ *
+ * Meant to allow you to make statements like:
+ *
+ * <pre class="stREPL">
+ *   result.value should be &gt; 15
+ * </pre>
+ *
+ * Where it only matches if result is both an `Either.Vaild` and has a value > 15.
+ *
+ * Otherwise your test will fail, indicating that it was an Invalid instead of Valid
+ *
+ * @see EitherValues.EitherValuable
+ */
+object EitherValues extends EitherValues
