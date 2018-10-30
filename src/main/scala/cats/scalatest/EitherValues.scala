8b3a9e2ce6ad09d33200fcb5642c38c37dcafd9d
Ben Plommer <ben.plommer@gmail.com>
2018-10-30 15:30:31 +0000

    Fix error messages for `EitherValues` (#23)
    * Fix grammar in error message
    
    * Correct error message for `EitherValues.leftValue`

--- src/main/scala/cats/scalatest/EitherValues.scala
+++ src/main/scala/cats/scalatest/EitherValues.scala
@@ -43,7 +43,7 @@ trait EitherValues {
       either match {
         case Right(right) => right
         case Left(left) =>
-          throw new TestFailedException((_: StackDepthException) => Some(s"'$left' is an Left, expected an Right."), None, pos)
+          throw new TestFailedException((_: StackDepthException) => Some(s"'$left' is a Left, expected a Right."), None, pos)
       }
     }
 
@@ -54,7 +54,7 @@ trait EitherValues {
     def leftValue: E = {
       either match {
         case Right(right) =>
-          throw new TestFailedException((_: StackDepthException) => Some(s"'$right' is Valid, expected Invalid."), None, pos)
+          throw new TestFailedException((_: StackDepthException) => Some(s"'$right' is a Right, expected a Left."), None, pos)
         case Left(left) => left
       }
     }
