4633838fc10c2ed76e4c36d8759f76aafcf5ffe7
Colt Frederickson <coltfred@gmail.com>
2022-10-27 16:19:00 -0600

    Add nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Provision a dev environment";
+
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+
+        mkShell = jdk: pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "http4s";
+          typelevelShell = {
+            jdk.package = jdk;
+            nodejs.enable = true;
+            native.enable = true;
+            native.libraries = [ pkgs.zlib pkgs.s2n-tls pkgs.openssl ];
+          };
+        };
+      in
+      rec {
+        devShell = devShells."temurin@11";
+
+        devShells = {
+          "temurin@11" = mkShell pkgs.temurin-bin-11;
+          "temurin@17" = mkShell pkgs.temurin-bin-17;
+        };
+      }
+    );
+}
