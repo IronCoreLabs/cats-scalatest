2527dc5d887e922a0f7c7e0217eb15bf943567d9
Colt Frederickson <coltfred@gmail.com>
2022-10-28 12:49:01 -0600

    Add flake.nix (#299)
    * Add nix
    
    * Add nix
    
    * Remove native

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Provision a dev environment";
+
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+
+        mkShell = jdk: pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "cats-scalatest";
+          typelevelShell = {
+            jdk.package = jdk;
+            nodejs.enable = true;
+          };
+        };
+      in
+      rec {
+        devShell = devShells."temurin@11";
+
+        devShells = {
+          "temurin@11" = mkShell pkgs.temurin-bin-11;
+          "temurin@17" = mkShell pkgs.temurin-bin-17;
+        };
+      }
+    );
+}
